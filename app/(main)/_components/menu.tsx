ba96d41ac777c04008eff3113c4e538990cb3bc8
Repith <119136592+Repith@users.noreply.github.com>
2023-10-24 16:10:32 +0200

    Note Menu | Toolbar | IconPicker
    - created
        + alert button for notes in trash
        + menu for each note
        + toolbar (add icon | add cover)
        + icon picker
    
    - convex
        + removeIcon API
    
    - added
        + icon picker
        + react-textarea-autosize

---
+++ app/(main)/_components/menu.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { MoreHorizontal, Trash } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface MenuProps {
+  documentId: Id<"documents">;
+}
+
+export const Menu = ({ documentId }: MenuProps) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Archiving...",
+      success: "Archived!",
+      error: "Failed to archive.",
+    });
+
+    router.push("/documents");
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        alignOffset={8}
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="text-xs text-muted-foreground p-2">
+          Last edited by {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return <Skeleton className="w-10 h-10" />;
+};
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
 import { MenuIcon } from "lucide-react";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -24,8 +26,11 @@ export const Navbar = ({
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -44,8 +49,14 @@ export const Navbar = ({
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && (
+        <Banner documentId={document._id} />
+      )}
     </>
   );
 };
